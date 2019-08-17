d87d66aeb4d839e81b3499bc78daa45cbfbc7418
willyam pinheiro <willfpinheiro@gmail.com>
2019-08-17 20:57:30 -0300

    Exercicios Mundo 3

--- aula16.py
+++ aula16.py
@@ -12,14 +12,35 @@ print(lanche[:2]) #mostra ate o 2
 print(lanche[:-2]) #mostra o inverso ate o fim do inverso
 print(lanche)
 print(len(lanche))
+#
+# for c in range(0, len(lanche)):
+#       print(f'eu vou comer {lanche[c]}')
+#
+# for comida in lanche:
+#      print(f'eu vou comer {comida}')
+#
+# for pos, comida in enumerate(lanche):
+#      print(f'eu vou comer {comida} na posição {pos+1}')
+# print('Eu comi muito')
 
-for c in range(0, len(lanche)):
-      print(f'eu vou comer {lanche[c]}')
+#Embaralhar uma tupla
+# print(sorted(lanche))
 
-for comida in lanche:
-     print(f'eu vou comer {comida}')
+#junçao de tuplas
+# a = (2, 5, 8)
+# b = (5, 9, 9, 8)
+# c = a + b
+# print(c)
+# print(c.count(9)) #conta quantos x aparece a informação no ()
+# print(c.index(8)) # Qual posição esta o valor
+# print(c.index(8, 3)) #qual posição a partir de tal posição
 
-for pos, comida in enumerate(lanche):
-     print(f'eu vou comer {comida} na posição {pos+1}')
+#Tuplas no pyton aceita qualquer tipos na mesma tupla
+pessoa = ('Will', 25, 'M', 75.2)
+print(pessoa)
 
-print('Eu comi muito')
+# Deletar tupla
+# nao podendo deletar um elemento de dentro dela, somente ela por inteira
+pessoa = ('Will', 25, 'M', 75.2)
+del(pessoa)
+print(pessoa)
