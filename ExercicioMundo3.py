973a70ead8384486f065debbd8ace8747f1f554d
willyam pinheiro <willfpinheiro@gmail.com>
2019-08-18 01:21:13 -0300

    Update ExercicioMundo3.py

--- ExercicioMundo3.py
+++ ExercicioMundo3.py
@@ -49,7 +49,7 @@ def ex075():
 
 
 def ex076():
-    num = ('Pão', 0.40, 'Açucar', 1.95, 'farinha', 2.40, 'Note', 1999.99)
+    num = ('Pão', 0.40, 'Açucar', 1, 'farinha', 2.40, 'Note', 1999.99)
     print('-' * 30)
     print('{: ^30}'.format('Listagem de Preços'))
     print('-' * 30)
@@ -57,7 +57,8 @@ def ex076():
         if pos % 2 == 0:
             print(f'{c:.<20}', end='')
         else:
-            print(f'R${c: >8}')
+            print(f'R${c: >8.2f}')
+
 
 def ex077():
     palavras = ('Palmeiras', 'remo', 'tuna', 'paysandu', 'Aguia de maraba', 'tabajara', 'Gremio', 'Cruzeiro', 'Atletico', 'Paranaense', 'Flamengo', 'Corinthians', 'Santos', 'Vasco da Gama', 'Bahia', 'Fluminense', 'Sao Paulo', 'Botafogo', 'Internacional', 'Atletico', 'Chapecoense')
