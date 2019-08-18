f9c21b413866952007eb00c2209f93257ceb2eb3
willyam pinheiro <willfpinheiro@gmail.com>
2019-08-18 00:58:11 -0300

    Update ExercicioMundo3.py

--- ExercicioMundo3.py
+++ ExercicioMundo3.py
@@ -2,19 +2,17 @@ import random
 def ex072():
     numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
     print(numero[0])
-    a = ''
     while True:
-        num = int(input(f'{a}Digite um valor entre 0 e 20: '))
+        num = int(input(f'Digite um valor entre 0 e 20: '))
         if 0 <= num < len(numero):
             break
-        else:
-            a = 'Tente Novamente. '
+        print('Tente Novamente. ', end='')
     print(f'Voce digitou o numero {numero[num]}')
 
 
 def ex073():
     brasileirao = ('Palmeiras', 'remo', 'tuna', 'paysandu', 'Aguia de maraba', 'tabajara', 'Grêmio', 'Cruzeiro', 'Atlético', 'Paranaense', 'Flamengo', 'Corinthians', 'Santos', 'Vasco da Gama', 'Bahia', 'Fluminense', 'São Paulo', 'Botafogo', 'Internacional', 'Atlético', 'Chapecoense')
-    print('-='*20)
+    print('-=' * 20)
     print(f'Os 5 primeiros colocados são: {brasileirao[0:5]}')
     print('-=' * 20)
     print(f'Os 4 ultimos são: {brasileirao[-4:]}')
