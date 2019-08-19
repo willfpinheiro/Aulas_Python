730f83f171b10a178ae298d5aebc8b003bb66b29
willyam pinheiro <willfpinheiro@gmail.com>
2019-08-18 22:42:41 -0300

    Update aula017.py

--- aula017.py
+++ aula017.py
@@ -28,12 +28,48 @@
 #Tamanho de uma lista
 # len(valores)
 
-num = [2, 3, 5, 1]
-num[2] = 1
-num.append(7) #adicionar no fim da
-num.sort()
-print(num)
-num.sort(reverse=True)
-num.insert(2, 100)
-num.pop(4)
-print(num)
+# num = [2, 3, 5, 1]
+# num[2] = 1
+# num.append(7) #adicionar no fim da
+# num.sort()
+# print(num)
+# num.sort(reverse=True)
+# num.insert(2, 100)
+# num.pop(4)
+# if 10 in num:
+#     num.remove(1) #so remove o primeiro 1
+# else:
+#     print('Não achei esse numero')
+# print(num)
+# print(f'Essa lista tem {len(num)} elementos')
+
+valores = []
+valores.append(5)
+valores.append(8)
+valores.append(4)
+
+for v in valores:
+    print(f'{v}, ', end='')
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei {v}!')
+print('Cheguei no final da lista')
+
+numeros = list()
+for cont in range(0, 5):
+    numeros.append((int(input('Digite um valor: '))))
+print(numeros)
+
+#Se igualar uma lista, faz uma migração, um link
+a = [1, 2, 3, 5]
+b = a
+b[2] = 100
+print(f'A: {a}')
+print(f'B: {b}')
+#Se quiser fazer uma copia precisa fazer
+c = [1, 2, 3, 5]
+d = c[:]
+d[2] = 100
+print(f'C: {c}')
+print(f'D: {d}')
+
