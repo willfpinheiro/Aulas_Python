81d706c85cfc228441b9c93584175f7a014a4a32
willyam pinheiro <willfpinheiro@gmail.com>
2019-08-27 13:39:05 -0300

    Update aula019.py

--- aula019.py
+++ aula019.py
@@ -1,29 +1,51 @@
-# Dicionarios - adiciona indices literais
-# Tuplas - ()
-# Listas - []
-# Dicionarios - {}
+# # Dicionarios - adiciona indices literais
+# # Tuplas - ()
+# # Listas - []
+# # Dicionarios - {}
+#
+# # Pode ser declara como:
+# dados = dict()
+# # ou
+# dados = {'nome':'Pedro', 'idade':25}
+# print(dados['nome'])
+# print (dados['idade'])
+# # Append não funciona no dicionario, para adicionar uma nova informação, precisa usar o:
+# dados['sexo'] = 'M'
+# # Para deletar um elemento, desssa forma sera eliminado tanto as informações quanto a coluna
+# del dados['idade']
+# filme = {'Titulo':'Star wars',
+#          'ano':1977,
+#          'diretor':'George Lucas'}
+# # Diferença entre VALOR X CHAVE X ITEM
+# # Valor é o conteudo de cada coluna
+# print(filme.values())
+# #Chaves é o nome de cada coluna
+# print (filme.keys())
+# # Item é tudo
+# print(filme.items())
+#
+# for chave, valor in filme.itens():
+#     print(f'O {chave} é {valor}')
 
-# Pode ser declara como:
-dados = dict()
-# ou
-dados = {'nome':'Pedro', 'idade':25}
-print(dados['nome'])
-print (dados['idade'])
-# Append não funciona no dicionario, para adicionar uma nova informação, precisa usar o:
-dados['sexo'] = 'M'
-# Para deletar um elemento, desssa forma sera eliminado tanto as informações quanto a coluna
-del dados['idade']
-filme = {'Titulo':'Star wars',
-         'ano':1977,
-         'diretor':'George Lucas'
-}
-# Diferença entre VALOR X CHAVE X ITEM
-# Valor é o conteudo de cada coluna
-print(filme.values())
-#Chaves é o nome de cada coluna
-print (filme.keys())
-# Item é tudo
-print(filme.items())
+#Comando basicos de um dicionario
 
-for chave, valor in filme.itens():
-    print(f'O {chave} é {valor}')
+# pessoas = {'nomes': 'Will', 'sexo': 'M', 'idade': 22}
+# print(f'{pessoas["nomes"]} tem {pessoas["idade"]} anos.') #Colocar aspas duplas pois ja possui aspa simples geral
+# print(pessoas.keys())
+# print(pessoas.values())
+# print(pessoas.items()) #mostra graficamente um lista compasta por tuplas
+#
+# for k in pessoas.keys():
+#     print(k)
+# for v in pessoas.values():
+#     print(v)
+# # Não tem enumerate, tendo que usar o items()
+# for k, v in pessoas.items():
+#     print(f'{k} = {v}')
+# # del pessoas['sexo']
+# pessoas['nomes'] = 'Leandro'
+# pessoas['peso'] = 70
+# for k, v in pessoas.items():
+#     print(f'{k} = {v}')
+
+#Colocando um dicionario em uma lista
